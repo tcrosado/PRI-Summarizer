cdb4745eab0959557d9f046722a1c77f624557cf
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2017-10-24 19:56:15 +0100

    Exercício 1 done

--- 1.py
+++ 1.py
@@ -1,41 +1,32 @@
-## ALL wrong maybe we can use some of these code later on####
+from nltk.tokenize import sent_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import operator
+file = open("example.in")
 
+document = file.read()
+# Turn into sentences
+sentences = sent_tokenize(document)
 
-import nltk
-nltk.download('punkt')
-from collections import defaultdict, Counter
-import math
+#TFIDF in each sentence
+vectorSpace = TfidfVectorizer(stop_words="english")
+resultSentence = vectorSpace.fit_transform(sentences)
 
-with open('data.txt', 'r') as myfile:			##Read a file
-    data=myfile.read().lower()
-data_words=Counter(nltk.word_tokenize(data)) ##Dictionary of the number that the words repeat themselfs in the Doc
-data_token=nltk.sent_tokenize(data)	##Split the sentences
+#print(resultSentence)
 
-DicDocument=defaultdict(list)
-DicSentence=defaultdict(list)
+#TFIDF of document
+resultDocument = vectorSpace.fit_transform([document])
 
-###################################################################################################
-count=0	
-###################################################################################################	
-				##get the frequency of each word in its sentence
-					#Number the sentences
+#print (resultDocument)
 
-for a in data_token:
-	dic_toadd=defaultdict(list)
-	aux_list=Counter(nltk.word_tokenize(a))
+scoreSentences = dict()
+for i in range(0,len(sentences)):
+    scoreSentences[i]=float(cosine_similarity(resultSentence[i:i+1],resultDocument))
 
-	for key,value in aux_list.iteritems():
-		dic_toadd[key].append(math.log(len(data)/aux_list.get(key)))
-	DicSentence[count].append(dic_toadd)
-	count=count+1
+sortedScore = sorted(scoreSentences.items(),key=operator.itemgetter(1),reverse=True)
 
-for key, value in data_words.iteritems(): 			#		## Iterate and put it in a list	
-	DicDocument[key].append(math.log(len(data)/value))
+keySentenceIDF = sortedScore[0:3]
+keySentences = sorted(keySentenceIDF)
 
-
-#print data_words
-print DicSentence
-print DicDocument
-
-###############################################################################################3
-#sklearn.metrics.pairwise.cosine_similarity
+for key in keySentences:
+    print(sentences[key[0]])
