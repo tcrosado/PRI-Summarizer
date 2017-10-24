6e5508a044cac3bc1d3d61038226fa6e3151180a
Fábio Gomes <fg@integrity.pt>
2017-10-24 14:00:09 +0100

    code added but some calculations are wrong

--- 1.1.py
+++ 1.1.py
@@ -2,6 +2,7 @@ import nltk
 nltk.download('punkt')
 from collections import defaultdict, Counter
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 
 with open('data.txt', 'r') as myfile:			##Read a file
     data=myfile.read().lower()
@@ -16,12 +17,24 @@ word_list=Counter(nltk.word_tokenize(data))
 for key, value in word_list.iteritems():
 	final_list.append(key)
 vec = CountVectorizer(vocabulary=final_list)
-data = vec.fit_transform(data_token).toarray()
-print data										####### creates a matrix where it put the number of times the words repeat of a a vocabulary wich in our case is the full doc
+sentences = vec.fit_transform(data_token).toarray()
+print sentences										####### creates a matrix where it put the number of times the words repeat of a a vocabulary wich in our case is the full doc
 
-
-######idf wow
 transformer = TfidfTransformer(smooth_idf=True)
-tfidf = transformer.fit_transform(data).toarray()
+tfidf = transformer.fit_transform(sentences).toarray()
 
 print tfidf
+###############################################################################################################
+full_document_matrix=vec.fit_transform(data_words)   ## will be needed to the cosine of similarity
+print full_document_matrix
+tfidf_full_document=transformer.fit_transform(full_document_matrix).toarray()
+###########################################################################################################
+######idf wow
+
+
+print cosine_similarity(tfidf[0:],tfidf_full_document)
+
+
+
+
+### ha calculos aqui a ser feitos mal
