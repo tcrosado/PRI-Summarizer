2a271bce9563086e094435ea22420f91e971e9fd
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2017-10-27 19:46:16 +0100

    Ex 2 summarizing and outputting

--- 2.py
+++ 2.py
@@ -13,37 +13,38 @@ enc = enc_PT
 #TFIDF in collection
 vectorSpace = TfidfVectorizer(encoding=enc)
 resultCollection = vectorSpace.fit_transform(files.data)
-
-#print (len(files.filenames))
+#print(files.data)
 sentences = []
+scoreSentences = []
 
 for file in files.filenames:
 	f = open(file, 'r', encoding=enc)
 	document = f.read()
 	# Turn into sentences
 	sentencesDocument = sent_tokenize(document)
+
+	#Little hack
+	if len(sentencesDocument)==0:
+		continue
+
 	sentences += sentencesDocument
 	#TFIDF in each sentence
 	resultSentence = vectorSpace.transform(sentencesDocument)
 
-	#print (resultDocument)
 
-	scoreSentences = []
-	for i in range(0,len(sentences)):
-#######################################################################################
-#######################################################################################
-	    scoreSentences.append(cosine_similarity(resultSentence[i:i+1],resultCollection)[0])
+
+	for i in range(0,len(sentencesDocument)):
+			scoreSentences.append(cosine_similarity(resultSentence[i:i+1],resultCollection))
 
 for i in range(len(files.filenames)):
 
-	scoreDoc = dict(zip(sentences, scoreSentences[i]))
-
+	scoreDoc = dict(zip(sentences, scoreSentences[i][0]))
 	sortedScore = sorted(scoreDoc.items(),key=operator.itemgetter(1),reverse=True)
 
-#######################################################################################
-#######################################################################################
-
+	#######################################################################################
+	#######################################################################################
 	keySentenceIDF = sortedScore[0:5]
+	# Order sentence by apperence in document
 	keySentences = sorted(keySentenceIDF)
 
 	nameFile = files.filenames[i] + '.out'
@@ -51,7 +52,9 @@ for i in range(len(files.filenames)):
 	output = open(nameFile, 'w', encoding=enc)
 
 	for key in keySentences:
-		print(sentences[key[0]])
-		outpu.twrite(sentences[key[0]])
+		#print(key)
+		#print(sentences[key[0]])
+		#output.write(sentences[key[0]])
+		output.write(key[0])
 
 	output.close()
