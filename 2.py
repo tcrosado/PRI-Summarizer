4fa4c1271c03cd07830a72ba67cc858b90e6a7d2
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2017-11-01 21:02:17 +0000

    Added Recall, Precision and F1 Score

--- 2.py
+++ 2.py
@@ -1,17 +1,18 @@
 from nltk.tokenize import sent_tokenize
+from nltk.metrics.scores import (precision, recall)
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.datasets import load_files
 import operator
 import re
-
+import os
 
 def getFilename(fullPath):
-    fileName = re.findall(r"/Text/(.*?)\.txt",fullPath)
-    if(fileName != []):
-        return fileName[0]
-    else:
-        return ""
+	fileName = re.findall(r"/Text/(.*?)\.txt",fullPath)
+	if(fileName != []):
+		return fileName[0]
+	else:
+		return ""
 
 def getNthMaxScores(n,scores):
 	result = []
@@ -22,6 +23,8 @@ def getNthMaxScores(n,scores):
 		scoreCopy.remove(maxNumber)
 	return result
 
+def calculateF1score(precision,recall):
+	return 2 * (precision * recall) / (precision + recall)
 
 
 
@@ -48,6 +51,12 @@ for file in files.filenames:
 	document = f.read()
 	# Turn into sentences
 	sentences[fileName] = sent_tokenize(document)
+	
+	# Parse Title as sentence
+	firstSentence = sentences[fileName][0]
+	sentences[fileName].remove(firstSentence) 
+
+	sentences[fileName] = firstSentence.split("\n") + sentences[fileName]
 
 	#TFIDF in each sentence
 	resultSentence = vectorSpace.transform(sentences[fileName])
@@ -65,7 +74,6 @@ for file in files.filenames:
 
 
 for fileName in scoreFileSentences.keys():
-	
 	maxScores = getNthMaxScores(5,allFileScores[fileName])
 	
 	lineScores = scoreFileSentences[fileName]
@@ -78,13 +86,40 @@ for fileName in scoreFileSentences.keys():
 				selectedLines.append(lineNumber)
 
 	sortedLines = sorted(selectedLines)
-	
-	filePath = pathFiles+"/Text/"+fileName + '.out'
+	filePath = pathFiles+"Text/"+fileName + '.out'
 	output = open(filePath, 'w', encoding=enc)
 
 	for lineNumber in sortedLines:
-		output.write(sentences[fileName][lineNumber])
+		#needs new line so nltk can split sentences correctly
+		output.write(sentences[fileName][lineNumber]+"\n")
 	
 	output.close()
 
-# Here should be the precision, recall and F1 and MAP calculations
+# Here should be the precision, recall and F1 and MA Pcalculations
+
+for fileName in scoreFileSentences.keys():
+	output = open(pathFiles+"Text/"+fileName+".out",'r', encoding=enc)
+	outputResult = output.read()
+	outputSentences = set(sent_tokenize(outputResult))
+	output.close()
+
+	expected = open(pathExpectedFiles+"Ext-"+fileName+".txt",'r', encoding=enc)
+	expectedResult = expected.readlines()
+	expectedSentences = []
+	for line in expectedResult:
+		expectedSentences += [line[:-1]]
+
+	expectedSentences = set(expectedSentences)
+	expected.close()
+	
+	recallResult = recall(expectedSentences,outputSentences)
+	precisionResult = precision(expectedSentences,outputSentences)
+	print("#")
+	print("file: ",fileName)
+	print("recall: ",recallResult)
+	print("precision: ",precisionResult)
+	if recallResult != 0 and precisionResult != 0:
+		print("F1 Score: ",calculateF1score(precisionResult,recallResult))
+
+#Clean Up
+os.system("rm "+pathFiles+"Text/*.out")
