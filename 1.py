1daf5b59516520e0825144bc5326b7c45af61915
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2017-11-02 19:12:44 +0000

    Refactored Ex1 so the same code can be used in Ex2

--- 1.py
+++ 1.py
@@ -1,32 +1,6 @@
-from nltk.tokenize import sent_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import operator
-file = open("example.in")
+from Ex1Lib import getSenteceBasedSummary
 
-document = file.read()
-# Turn into sentences
-sentences = sent_tokenize(document)
+summary = getSenteceBasedSummary(TfidfVectorizer(stop_words="english"),"example.in",3)
 
-#TFIDF in each sentence
-vectorSpace = TfidfVectorizer(stop_words="english")
-resultSentence = vectorSpace.fit_transform(sentences)
-
-#print(resultSentence)
-
-#TFIDF of document
-resultDocument = vectorSpace.fit_transform([document])
-
-#print (resultDocument)
-
-scoreSentences = dict()
-for i in range(0,len(sentences)):
-    scoreSentences[i]=float(cosine_similarity(resultSentence[i:i+1],resultDocument))
-
-sortedScore = sorted(scoreSentences.items(),key=operator.itemgetter(1),reverse=True)
-
-keySentenceIDF = sortedScore[0:3]
-keySentences = sorted(keySentenceIDF)
-
-for key in keySentences:
-    print(sentences[key[0]])
+print(summary)
