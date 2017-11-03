5dcb308147b04b50e74093af6ef31dbef754b9a2
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2017-11-03 16:02:40 +0000

    Ex4 using MMR. Needs comparission with ex2 and test changes in lambda

--- 4-alternative.py
+++ 4-alternative.py
@@ -1,26 +1,32 @@
 from nltk.tokenize import sent_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.datasets import load_files
 import operator
-import glob
 
+
+enc_PT = 'ISO-8859-15'
+enc_US = 'UTF-8'
+enc = enc_PT
 
 def calculateMMR(lamb,document,sentence,selectedSentences):
-	
+
 	simSum = 0
 	for selectedSentence in selectedSentences:
 		simSum+=cosine_similarity(sentence,selectedSentence)
 
-	return (1 - lamb) * cosine_similarity(sentence,document) - lamb * simSum 
+	return (1 - lamb) * cosine_similarity(sentence,document) - lamb * simSum
 
 
-resume=dict()	
+resume=dict()
 
 number=0
 
-for p in glob.glob('../TeMario/*.txt'):
+files = load_files('../TeMario/')
 
-	file = open(p)
+for fileName in files.filenames:
+
+	file = open(fileName, 'r', encoding=enc)
 
 	document = file.read()
 	# Turn into sentences
@@ -33,63 +39,28 @@ for p in glob.glob('../TeMario/*.txt'):
 	#print(resultSentence)
 
 	#TFIDF of document
+	# This should be for all files ?
 	resultDocument = vectorSpace.fit_transform([document])
 
-	#Selecting the first sentence
-	maxScore = 0
-	sentenceIndex = -1
-	for i in range(0,len(sentences)):
-	   	similarity = float(cosine_similarity(resultSentence[i:i+1],resultDocument))
-	   	if similarity > maxScore:
-	   		maxScore = similarity
-	   		sentenceIndex = i
 
-	selectedSentences = [sentences[sentenceIndex]]
-	for i in range(4):
-		lamb = 0.2 #Example
-		calculateMMR(lamb,resultDocument,sentence,selectedSentences)
+	#FIXME Has problems parsing first sentence
 
+	lamb = 0.7
+	summarySize = 5
+	selectedSentences = []
+	selectedSentencesScores = []
+	while len(selectedSentences) != summarySize:
 
+		score = dict()
+		for i in range(0,len(sentences)):
+			score[i] = calculateMMR(lamb,resultDocument,resultSentence[i],selectedSentencesScores)
 
+		lineNumber = max(score.items(), key=operator.itemgetter(1))[0]
+		selectedSentencesScores += [resultSentence[lineNumber]]
+		selectedSentences+=[sentences[lineNumber]]
 
-
-
-
-
-
-
-
-
-
-	scoreSentences = dict()
-	for i in range(0,len(sentences)):
-	    scoreSentences[i]=float(cosine_similarity(resultSentence[i:i+1],resultDocument))
-
-	sortedScore = sorted(scoreSentences.items(),key=operator.itemgetter(1),reverse=True)
-
-
-
-
-
-
-
-	#print (resultDocument)
-
-
-
-	keySentenceIDF = sortedScore[0:3]
-	keySentences = sorted(keySentenceIDF)
-
-	total=[]
-	total.append(sortedScore[0])
-	for a in sortedScore:
-
-		if len(total)<5 and total[-1][1]!=a[1]:
-			total.append(a)
-		
-		if total ==5:
-			break
-	
-	resume[number]=total
-	number+=1
-print(resume)
+	#while select line != max
+	#run MMR
+	#max score and select line
+	print("File:",fileName)
+	print(selectedSentences)
