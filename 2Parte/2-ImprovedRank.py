271068314eab8099b071243ed58904e159fdb084
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2017-12-06 18:58:15 +0000

    Added  metrics from 1st deliver Added NounPhrase calculation for weight calculation Fixed Non uniform prior based on cosineSim

--- 2Parte/2-ImprovedRank.py
+++ 2Parte/2-ImprovedRank.py
@@ -1,18 +1,33 @@
 from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.tree import Tree
+from  nltk import pos_tag
+from  nltk import RegexpParser
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.datasets import load_files
 from graph import Graph
+from metrics import getMetrics
+from metrics import getFilename
 
 enc_PT = 'ISO-8859-15'
 enc_US = 'UTF-8'
 enc = enc_PT
 
 pathFiles = "../../TeMario/"
+pathExpectedFiles = "../../idealTeMario/"
 
 files = load_files(pathFiles)
 
-fileName = files.filenames[0]
+summaries = []
+
+def getOutputFilePath(fileName):
+    return pathFiles+"Text/"+fileName + '.out'
+
+def getExpectedFilePath(fileName):
+    return pathExpectedFiles+"Ext-"+fileName+".txt"
+
+
 
 def getSentencesFromFile(filePath):
     file = open(filePath, 'r', encoding=enc)
@@ -53,62 +68,129 @@ def calcPageRankWPrior(nodeName,pageRank,graph,damping,priorFunc,weightFunc):
 
     return pr
 
-#Creating a sentence list
-sentList = getSentencesFromFile(fileName)
-
-def getConsineSimFromSent(sentence1,sentence2):
-        vectorSpace = TfidfVectorizer(encoding=enc)
-        resultSentences = vectorSpace.fit_transform(sentList)
-        i = sentList.index(sentence1)
-        j = sentList.index(sentence2)
-        return cosine_similarity(resultSentences[i],resultSentences[j])
-
-def getCosineSimSentenceDocument(sentence):
-    #FIXME: Not working
-    vectorSpace = TfidfVectorizer(encoding=enc)
-    f = open(fileName, 'r', encoding=enc)
-    document = f.read()
-    resultDoc = vectorSpace.fit_transform(document)
-    resultSentence = vectorSpace.transform(sentList)
-    i = sentList.index(sentence)
-    return cosine_similarity(resultSentence[i],resultDoc)
 
 
 
-def createGraph(sentenceList,treshold):
-    graph = Graph()
 
-    for i in range(len(sentenceList)):
-        for j in range(i+1,len(sentenceList)):
+for filePath in files.filenames:
+    fileName = getFilename(filePath)
+    #Creating a sentence list
+    sentList = getSentencesFromFile(filePath)
+
+    # ################# Weight assignment ################
+    def getConsineSimFromSent(sentence1,sentence2):
             vectorSpace = TfidfVectorizer(encoding=enc)
-            resultSentences = vectorSpace.fit_transform(sentenceList)
+            resultSentences = vectorSpace.fit_transform(sentList)
+            i = sentList.index(sentence1)
+            j = sentList.index(sentence2)
+            return cosine_similarity(resultSentences[i],resultSentences[j])[0][0]
 
-            sim = cosine_similarity(resultSentences[i],resultSentences[j])
+    def getNounPhraseBasedWeight(sentence1,sentence2):
+        # weight = number of noun phrases shared between the sentences
+        def getNounPhrases(sentences):
+            nounPhrases = []
+            words = [word_tokenize(sentence) for sentence in sentences]
+            taggedWords = [pos_tag(word) for word in words]
+            for sent in taggedWords:
+                grammar = r"""
+                NBAR:
+                    {<NN.*|JJ>*<NN.*>}  # Nouns and Adjectives, terminated with Nouns
 
-            if(sim>=treshold):
-                graph.addBiEdge(sentList[i],sentList[j])
-    return graph
+                NP:
+                    {<NBAR>}
+                    {<NBAR><IN><NBAR>}"""
+                parser = RegexpParser(grammar)
+                sentence = parser.parse(sent)
+
+                grammar = "NP: {<NN>+}"
+                parser = RegexpParser(grammar)
+                classTree = parser.parse(sentence)
+
+                flatTree = []
+                for leaf in classTree:
+                    flatTree.append(leaf)
+
+                for leaf in flatTree:
+                    if isinstance(leaf,Tree) and leaf.label() == 'NP':
+                        nounPhrase= ''
+                        for (word,cls) in leaf.leaves():
+                            nounPhrase+=" "+word
+                        nounPhrases.append(nounPhrase)
+
+            return nounPhrases
+
+        npSentence1 = getNounPhrases([sentence1])
+        npSentence2 = getNounPhrases([sentence2])
+
+        weight = 1
+        for np in npSentence1:
+            if np in npSentence2:
+                weight+=1
+
+        return weight
 
 
-treshold = 0.2
-graph = createGraph(sentList,treshold)
 
-pageRank = dict()
+    # ################ Prior Probability assignment ###############
+    def getCosineSimSentenceDocument(sentence):
 
-for sentence in sentList:
-    pageRank[sentence] = 1/len(sentList)
-
-for sentence in sentList:
-    pr =calcPageRankWPrior(sentence,pageRank,graph,0.15,
-        sentList.index,getConsineSimFromSent)
-    #pr =calcPageRankWPrior(sentence,pageRank,graph,0.15,
-     #   getCosineSimSentenceDocument,getConsineSimFromSent)
-
-    pageRank[sentence] = pr if pr==0 else pr[0][0]
+        vectorSpace = TfidfVectorizer(encoding=enc)
+        f = open(filePath, 'r', encoding=enc)
+        document = f.read()
+        resultDoc = vectorSpace.fit_transform([document])
+        resultSentence = vectorSpace.transform(sentList)
+        i = sentList.index(sentence)
+        return cosine_similarity(resultSentence[i],resultDoc)
 
 
-print(sorted(pageRank, key=pageRank.__getitem__)[:5])
+    #################################################################
 
+    def createGraph(sentenceList,treshold):
+        graph = Graph()
+
+        for i in range(len(sentenceList)):
+            for j in range(i+1,len(sentenceList)):
+                vectorSpace = TfidfVectorizer(encoding=enc)
+                resultSentences = vectorSpace.fit_transform(sentenceList)
+
+                sim = cosine_similarity(resultSentences[i],resultSentences[j])
+
+                if(sim>=treshold):
+                    graph.addBiEdge(sentList[i],sentList[j])
+        return graph
+
+
+    treshold = 0.2
+    graph = createGraph(sentList,treshold)
+
+    pageRank = dict()
+
+    for sentence in sentList:
+        pageRank[sentence] = 1/len(sentList)
+
+    for sentence in sentList:
+        #pr =calcPageRankWPrior(sentence,pageRank,graph,0.15,
+        #    sentList.index,getConsineSimFromSent)
+
+        #pr =calcPageRankWPrior(sentence,pageRank,graph,0.15,
+        #    sentList.index,getNounPhraseBasedWeight)
+
+        #pr =calcPageRankWPrior(sentence,pageRank,graph,0.15,
+        #   getCosineSimSentenceDocument,getConsineSimFromSent)
+
+        pr =calcPageRankWPrior(sentence,pageRank,graph,0.15,
+           getCosineSimSentenceDocument,getNounPhraseBasedWeight)
+
+        pageRank[sentence] = pr
+
+    summaries += sorted(pageRank, key=pageRank.__getitem__)[:5]
+
+    resultFilePath = getOutputFilePath(fileName+"D")
+    expectedFilePath = getExpectedFilePath(fileName)
+    with open(resultFilePath,"w", encoding = enc) as f:
+        f.write("\n".join(summaries))
+
+    getMetrics(fileName,resultFilePath,expectedFilePath,enc)
 
 
 
